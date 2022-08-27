16929161f76bc41ec5cf0443e4248e2a8fba5b05
Bikram Dutta <duttabikram260@gmail.com>
2022-08-28 00:27:45 +0530

    bikram files day-5

---
+++ components/blog_footer.js
@@ -0,0 +1,58 @@
+function footer(){
+    return `<div class="footer_top">
+    <div style="width:30% ;">
+        <img style="height:32px ;" src="https://buffer.com/resources/content/images/2021/02/buffer-logo.svg"
+            alt="">
+    </div>
+    <div class="options1">
+        <h3>Categories</h3>
+        <p>Social Media Marketing</p>
+        <p>Instagram Marketing</p>
+        <p>Social Analytics</p>
+        <p>News and Trends</p>
+        <p>Best apps and tools</p>
+        <p>Inside Buffer</p>
+        <p>Remote Work</p>
+        <p>Transparency</p>
+    </div>
+    <div class="options1">
+        <h3>Blog & podcasts</h3>
+        <P>Science of Social Media</P>
+        <P>Breaking Brand</P>
+        <P>Social Media Blog</P>
+        <P>Open Blog</P>
+    </div>
+    <div class="options1">
+        <h3>About Buffer</h3>
+        <p>Social media solutions</p>
+        <p>Our culture</p>
+        <p>Our values</p>
+    </div>
+</div>
+
+<div class="footer_btm">
+    <div class="logo2">
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-pinterest"></i><br>
+        <span>Copyright © 2022 Buffer &nbsp; |</span>
+        <span>Privacy &nbsp; |</span>
+        <span>Terms &nbsp; |</span>
+        <span>Security &nbsp; </span>
+
+    </div>
+    <div style="display:flex ;">
+        <div>
+            <span style="position:relative ; top:10px; font-weight: 500;">Get started with Buffer for
+                free</span>
+        </div>
+        <div>
+            <button id="signupbtn2">Sign up now</button>
+        </div>
+    </div>
+</div>`
+}
+
+export default footer;
