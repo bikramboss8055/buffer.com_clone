6ac0747e3caf3b96f9f6fe9e6d46505b93e7cc36
Shivareddykyadari <kyadarishivareddy1430@gmail.com>
2022-08-27 22:56:33 +0530

    shiva-day5-Files

--- guides.js
+++
@@ -1,8 +0,0 @@
-// let cont=document.querySelector("#head");
-// let div=document.createElement("div")
-// let h1=document.createElement("h1")
-// h1.textContent="Guides & course"
-// let p1=document.createElement("p");
-// p1.textContent="A collection of posts on Guided Courses"
-// div.append(h1,p)
-// cont.append(div)
